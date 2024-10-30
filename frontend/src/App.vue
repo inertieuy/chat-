d76bb984b97453eead8ba06ab58d4156157038a2
yogi nugroho <Ugroho@proton.me>
2024-10-30 23:49:27 +0700

    css

---
+++ frontend/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Home from './views/home/home.vue';
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
---
+++ frontend/src/views/user/dashboard.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from '../../components/user/navbar.vue';
+</script>
+
+<template>
+  <Navbar />
+  <div class="card bg-primary text-primary-content w-96">
+    <div class="card-body">
+      <h2 class="card-title">Card title!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn">Buy Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
